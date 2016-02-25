e1a46c02100070b6eda0f4f75d0a7d4ab4b02596
chyyuu <yuchen@tsinghua.edu.cn>
2016-02-25 23:59:00 +0800

    add dissambler tool with the big help from Wang Cheng Rui"

--- build-funcall.sh
+++ build-funcall.sh
@@ -2,7 +2,9 @@
 rm -f xc xem funcall funcall.txt
 gcc -o xc -O3 -m32 -Ilinux -Iroot/lib root/bin/c.c
 gcc -o xem -O3 -m32 -Ilinux -Iroot/lib root/bin/em.c -lm
+gcc -o dis -O3 root/bin/dis.c
 ./xc -o funcall -Iroot/lib root/usr/funcall.c
 ./xc -s -Iroot/lib root/usr/funcall.c >funcall.txt
+./dis funcall -o funcall.s -c
 ./xem funcall
 
---
+++ root/bin/dis.c
@@ -0,0 +1,238 @@
+//#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+//using namespace std;
+
+char ops[] =
+  "HALT,ENT ,LEV ,JMP ,JMPI,JSR ,JSRA,LEA ,LEAG,CYC ,MCPY,MCMP,MCHR,MSET," // system
+  "LL  ,LLS ,LLH ,LLC ,LLB ,LLD ,LLF ,LG  ,LGS ,LGH ,LGC ,LGB ,LGD ,LGF ," // load a
+  "LX  ,LXS ,LXH ,LXC ,LXB ,LXD ,LXF ,LI  ,LHI ,LIF ,"
+  "LBL ,LBLS,LBLH,LBLC,LBLB,LBLD,LBLF,LBG ,LBGS,LBGH,LBGC,LBGB,LBGD,LBGF," // load b
+  "LBX ,LBXS,LBXH,LBXC,LBXB,LBXD,LBXF,LBI ,LBHI,LBIF,LBA ,LBAD,"
+  "SL  ,SLH ,SLB ,SLD ,SLF ,SG  ,SGH ,SGB ,SGD ,SGF ,"                     // store
+  "SX  ,SXH ,SXB ,SXD ,SXF ,"
+  "ADDF,SUBF,MULF,DIVF,"                                                   // arithmetic
+  "ADD ,ADDI,ADDL,SUB ,SUBI,SUBL,MUL ,MULI,MULL,DIV ,DIVI,DIVL,"
+  "DVU ,DVUI,DVUL,MOD ,MODI,MODL,MDU ,MDUI,MDUL,AND ,ANDI,ANDL,"
+  "OR  ,ORI ,ORL ,XOR ,XORI,XORL,SHL ,SHLI,SHLL,SHR ,SHRI,SHRL,"
+  "SRU ,SRUI,SRUL,EQ  ,EQF ,NE  ,NEF ,LT  ,LTU ,LTF ,GE  ,GEU ,GEF ,"      // logical
+  "BZ  ,BZF ,BNZ ,BNZF,BE  ,BEF ,BNE ,BNEF,BLT ,BLTU,BLTF,BGE ,BGEU,BGEF," // conditional
+  "CID ,CUD ,CDI ,CDU ,"                                                   // conversion
+  "CLI ,STI ,RTI ,BIN ,BOUT,NOP ,SSP ,PSHA,PSHI,PSHF,PSHB,POPB,POPF,POPA," // misc
+  "IVEC,PDIR,SPAG,TIME,LVAD,TRAP,LUSP,SUSP,LCL ,LCA ,PSHC,POPC,MSIZ,"
+  "PSHG,POPG,NET1,NET2,NET3,NET4,NET5,NET6,NET7,NET8,NET9,"
+  "POW ,ATN2,FABS,ATAN,LOG ,LOGT,EXP ,FLOR,CEIL,HYPO,SIN ,COS ,TAN ,ASIN," // math
+  "ACOS,SINH,COSH,TANH,SQRT,FMOD,"
+  "IDLE,";
+ 
+struct Header {
+	int magic;
+	int bss;
+	int entry;
+	int flags;
+};
+struct Header hdr;
+
+int dat[65536];
+int label[65536];
+bool cmt = false;
+
+void usage() {
+	printf("disasm Exec_File -o Assemble_File [-c]\n");
+	printf("  -c : print comments for each instrs\n");
+}
+
+int main(int argc, char** argv) {
+	char *file_i, *file_o;
+	
+	if (argc<=1) {
+            usage();
+	        exit(1);
+	}
+	
+	for (int i = 1 ; i < argc ; i++) {
+		if (!strcmp(argv[i], "-help") || !strcmp(argv[i], "-h")){
+            usage();
+		} else if (!strcmp(argv[i], "-c"))
+			cmt = true;
+		else if (!strcmp(argv[i], "-o"))
+			file_o = argv[++i];
+		else
+			file_i = argv[i];
+	}
+	FILE* fi = fopen(file_i, "rb");
+    if (fi==NULL) {
+            printf("Can not open input file %s\n",file_i);
+            exit(-1);
+    }
+    
+	FILE* fo = fopen(file_o, "wb");
+    if (fo==NULL) {
+            printf("Can not open output file %s\n",file_o);
+            exit(-1);
+    }
+        	
+	fread(&hdr, 1, sizeof(hdr), fi);
+	printf("magic = 0x%08x\n", hdr.magic);
+	if(hdr.magic!=0xc0def00d) {
+            printf("%s is not a legal v9-cpu exec file\n",file_i);
+            exit(-1);
+    }
+    	
+	printf("bss = %d\n", hdr.bss);
+	int c_sz = (fread(&dat, 1, sizeof(dat), fi)>>2)-(hdr.bss>>1);
+	int label_sz = 0, label_ct = 0;
+	for (int i = 0 ; i < c_sz ; i++) {
+		int inst = dat[i]&255;
+		int imme = dat[i]>>8;
+		if (ops[inst*5] == 'B' && ops[inst*5+1] != 'I' && ops[inst*5+1] != 'O') {
+			printf("b to %d\n", i+(imme>>2)+1);
+			label[i+(imme>>2)+1] = ++label_sz;
+		}
+		if (ops[inst*5] == 'J') {
+			printf("j to %d\n", i+(imme>>2)+1);
+			label[i+(imme>>2)+1] = ++label_sz;
+		}
+	}
+	fprintf(fo, "No.:   Addr:    Value:  Intr  Operand  (Dec Format) #Label_id #Meaning\n");
+	fprintf(fo, "=======================================================================\n");	 
+	for (int i = 0 ; i < c_sz ; i++) {
+		int inst = dat[i]&255;
+		int imme = (int)(((unsigned int)(dat[i]))>>8);
+		fprintf(fo, "%3d: %08x: %08x: %c%c%c%c 0x%06x (D%4d)",
+			i+1, i*4, dat[i],
+			ops[inst*5], ops[inst*5+1], ops[inst*5+2], ops[inst*5+3],
+			imme, dat[i]>>8
+		);
+		if (i == hdr.entry>>2)
+			fprintf(fo, " # <= ENTRY ");
+		if (label[i])
+			fprintf(fo, " # label%d ", label[i]);
+		else
+			fprintf(fo, "          ");
+		if (ops[inst*5] == 'B' && ops[inst*5+1] != 'I' && ops[inst*5+1] != 'O')
+			fprintf(fo, " # Cond goto label%d", ++label_ct);
+		else if (ops[inst*5] == 'J') {
+		        if(ops[inst*5+1] == 'M') {
+			        fprintf(fo, " # Jmp label%d", ++label_ct);
+			    } else if(ops[inst*5+1] == 'S') {
+			        fprintf(fo, " # Call label%d", ++label_ct);
+			    }
+		}	    
+		if (!cmt) {
+			fprintf(fo, "\n"); continue;
+		}
+		if (inst >= 0x4f && inst <= 0x83) {
+			if (inst-0x4f < 4) {
+				switch (inst-0x4f) {
+					case 0 : fprintf(fo, " # rf = rf + rg"); break;
+					case 1 : fprintf(fo, " # rf = rf - rg"); break;
+					case 2 : fprintf(fo, " # rf = rf * rg"); break;
+					case 3 : fprintf(fo, " # rf = rf / rg"); break;
+				}
+			} else if ((inst-0x54)/3 < 13) {
+				switch ((inst-0x54)/3) {
+					case 0 : fprintf(fo, " # ra = ra +"); break;
+					case 1 : fprintf(fo, " # ra = ra -"); break;
+					case 2 : fprintf(fo, " # ra = int(ra) * "); break;
+					case 3 : fprintf(fo, " # ra = int(ra) /"); break;
+					case 4 : fprintf(fo, " # ra = uint(ra) /"); break;
+					case 5 : fprintf(fo, " # ra = int(ra) mod"); break;
+					case 6 : fprintf(fo, " # ra = uint(ra) mod"); break;
+					case 7 : fprintf(fo, " # ra = ra &"); break;
+					case 8 : fprintf(fo, " # ra = ra |"); break;
+					case 9 : fprintf(fo, " # ra = ra ^"); break;
+					case 10: fprintf(fo, " # ra = ra <<"); break;
+					case 11: fprintf(fo, " # ra = int(ra) >>"); break;
+					case 12: fprintf(fo, " # ra = uint(ra) >>"); break;
+				}
+				switch ((inst-0x54)%3) {
+					case 0 : fprintf(fo, " rb"); break;
+					case 1 : fprintf(fo, " %d", imme); break;
+					case 2 : fprintf(fo, " sp[%d]", imme); break;
+				}
+			} else {
+				switch (inst-0x7a) {
+					case 0 : fprintf(fo, " # ra = (ra == rb)"); break;
+					case 1 : fprintf(fo, " # ra = (rf == rg)"); break;
+					case 2 : fprintf(fo, " # ra = (ra != rb)"); break;
+					case 3 : fprintf(fo, " # ra = (rf != rg)"); break;
+					case 4 : fprintf(fo, " # ra = (ra == rb)"); break;
+					case 5 : fprintf(fo, " # ra = ((int)ra < (int)rb)"); break;
+					case 6 : fprintf(fo, " # ra = ((uint)ra < (uint)rb)"); break;
+					case 7 : fprintf(fo, " # ra = (f < g)"); break;
+					case 8 : fprintf(fo, " # ra = ((int)ra < (int)rb)"); break;
+					case 9 : fprintf(fo, " # ra = ((uint)ra < (uint)rb)"); break;
+					case 10: fprintf(fo, " # ra = (f > g)"); break;
+				}
+			}
+		}
+		if (ops[inst*5] == 'L') {
+			if (ops[inst*5+2] == 'V') {
+				fprintf(fo, "\n"); continue;
+			}
+			if (ops[inst*5+1] == 'E') {
+				if (ops[inst*5+3] == 'G') 
+					fprintf(fo, " # ra = pc+%d\n", imme);
+				else
+					fprintf(fo, " # ra = sp+%d\n", imme);
+				continue;
+			}
+			fprintf(fo, " # ");
+			int opi = inst*5+1;
+			if (ops[opi] == 'B') {
+				fprintf(fo, "rb = ");
+				opi++;
+			} else
+				fprintf(fo, "ra = ");
+			switch (ops[opi+1]) {
+				case ' ' : fprintf(fo, "uint(");  break;
+				case 'S' : fprintf(fo, "short("); break;
+				case 'H' : fprintf(fo, "ushort(");break;
+				case 'C' : fprintf(fo, "char(");  break;
+				case 'B' : fprintf(fo, "uchar("); break;
+				case 'D' : fprintf(fo, "double(");break;
+				case 'F' : fprintf(fo, "float("); break;
+			}
+			switch (ops[opi]) {
+				case 'L' : fprintf(fo, "sp[%d])",    imme); break;
+				case 'G' : fprintf(fo, "gaddr[%d])", imme); break;
+				case 'X' : fprintf(fo, "v2p(%x06d))",imme); break;
+				case 'I' : fprintf(fo, "%d)", 		 imme); break;
+			}
+		}
+		if (ops[inst*5] == 'S') {
+			if (ops[inst*5+1] == 'U') {
+				fprintf(fo, "\n"); continue;
+			}
+			fprintf(fo, " # ");
+			int opi = inst*5+1;
+			if (ops[opi] == 'B') opi++;
+			switch (ops[opi+1]) {
+				case ' ' : fprintf(fo, "uint(");  break;
+				case 'S' : fprintf(fo, "short("); break;
+				case 'H' : fprintf(fo, "ushort(");break;
+				case 'C' : fprintf(fo, "char(");  break;
+				case 'B' : fprintf(fo, "uchar("); break;
+				case 'D' : fprintf(fo, "double(");break;
+				case 'F' : fprintf(fo, "float("); break;
+			}
+			switch (ops[opi]) {
+				case 'L' : fprintf(fo, "sp[%d])",    imme); break;
+				case 'G' : fprintf(fo, "gaddr[%d])", imme); break;
+				case 'X' : fprintf(fo, "v2p(%x06d))",imme); break;
+			}
+			if (opi == inst*5+2)
+				fprintf(fo, " = rb");
+			else
+				fprintf(fo, " = ra");
+		}
+		fprintf(fo, "\n");
+	}
+	return 0;
+}
+
+
