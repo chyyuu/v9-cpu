ca7c417e423af1d5f16f6d7038de7b237477af1e
chyyuu <yuchen@tsinghua.edu.cn>
2016-02-26 08:13:17 +0800

    update gitignore and build-funcall.sh

--- build-funcall.sh
+++ build-funcall.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-rm -f xc xem funcall funcall.txt
+rm -f xc xem dis funcall funcall.txt funcall.s
 gcc -o xc -O3 -m32 -Ilinux -Iroot/lib root/bin/c.c
 gcc -o xem -O3 -m32 -Ilinux -Iroot/lib root/bin/em.c -lm
 gcc -o dis -O3 root/bin/dis.c
