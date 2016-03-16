0aef40a9505ae1664be3dc547d59de010a0c360c
chyyuu <yuchen@tsinghua.edu.cn>
2016-03-16 13:36:38 +0800

    fix bug: btN --> btN-1 in some place, move backtrace maintain parts to the end of LABEL runir

--- root/bin/em.c
+++ root/bin/em.c
@@ -689,12 +689,12 @@ void func_entry_match(uint *insts, int instN, char *codes, int codeL) {
       }
     }
   }
-
-  for (i = 0; i < stuN; i++) {
-    printf("%s size=%d\n", stus[i].name, stus[i].size);
-    for (j = 0; j < stus[i].numN; j++)
-      printf("%s %s %s\n", stus[i].name, stus[i].nums[j].type, stus[i].nums[j].name);
-  }
+//for show more info
+//  for (i = 0; i < stuN; i++) {
+//    printf("%s size=%d\n", stus[i].name, stus[i].size);
+//    for (j = 0; j < stus[i].numN; j++)
+//      printf("%s %s %s\n", stus[i].name, stus[i].nums[j].type, stus[i].nums[j].name);
+//  }
 }
 
 void printi(int addr, int tpc) {
@@ -1296,6 +1296,7 @@ void cpu(uint pc, uint sp) {
     if ((uint) xpc == fpc) {
       fixpc:
       //printf("b1\n");
+      //printf("fixpc\n");
       if (!(p = tr[(v = (uint) xpc - tpc) >> 12]) && !(p = rlook(v))) {
         trap = FIPAGE;
         goto exception;
@@ -1303,22 +1304,8 @@ void cpu(uint pc, uint sp) {
       xcycle -= tpc;
       xcycle += (tpc = (uint)(xpc = (int *) (v ^ (p - 1))) - v);
       fpc = ((uint) xpc + 4096) & -4096;
-      switch ((uchar) ir) {
-        case JSR :
-          ipos = (uint) xpc - tpc;
-          cpos = 0;
-          while (cpos < stmtN && (stmts[cpos].start == stmts[cpos].end || ipos >= stmts[cpos].end)) cpos++;
-          for (i = 0; i < funcN - 1 && func[i].end < cpos; i++);
-          bts[btN].func = i;
-          bts[btN].stmt = cpos - func[i].line;
-          bts[btN++].sp = xsp - tsp;
-          break;
-        case LEV :
-          btN--;
-          break;
-      }
       next:
-      //printf("b2\n");
+      //printf("next\n");
       if ((uint) xpc > xcycle) {
         cycle += delta;
         xcycle += delta * 4;
@@ -1360,6 +1347,32 @@ void cpu(uint pc, uint sp) {
     }
 
     runir:
+    // =====> new feature : backtrace maintenance (push when JSR(A) / pop when LEV)
+    switch ((uchar) ir) {
+      case JSR :
+      case JSRA :
+        ipos = (uint)(xpc) - tpc;
+        cpos = 0;
+        while (cpos < stmtN && (stmts[cpos].start == stmts[cpos].end || ipos >= stmts[cpos].end)) cpos++;
+        for (i = 0; i < funcN - 1 && func[i].end < cpos; i++);
+        bts[btN].func = i;
+        bts[btN].stmt = cpos - func[i].line;
+        bts[btN++].sp = xsp - tsp;
+        //printf("btN=%d func=%d stmt=%d sp=%d\n",
+        //	btN, bts[btN-1].func, bts[btN-1].stmt, bts[btN-1].sp);
+        break;
+      case LEV :
+        if (!--btN) {
+          for (i = 0; i < funcN; i++)
+            if (!strcmp(func[i].nameS, "mainc")) break;
+          bts[0].func = i;
+          bts[0].stmt = 0;
+          bts[0].sp = xsp - tsp;
+          btN = 1;
+        }
+        break;
+    }
+// <=====
     ir = *xpc++;
     //printi((uint)(xpc-1)-tpc, tpc);
     //printf("pc = %x\n", (uint)(xpc-1)-tpc);
@@ -1627,9 +1640,9 @@ void cpu(uint pc, uint sp) {
     }
 
     ipos = (uint)(xpc - 1) - tpc;
-    cpos = func[bts[btN].func].line + bts[btN].stmt;
+    cpos = func[bts[btN - 1].func].line + bts[btN - 1].stmt;
     while (cpos < stmtN && (stmts[cpos].start == stmts[cpos].end || ipos >= stmts[cpos].end)) cpos++;
-    bts[btN].stmt = cpos - func[bts[btN].func].line;
+    bts[btN - 1].stmt = cpos - func[bts[btN - 1].func].line;
 
     switch ((uchar) ir) {
       case HALT:
